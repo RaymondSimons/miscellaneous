3615a60569b411c8bf24209a96e474cb4f0fa91e
RaymondSimons <rsimons@pha.jhu.edu>
2016-09-14 12:19:18 -0400

    testing camera positions from VELA/YT pipeline

--- check_camera_positions.py
+++ check_camera_positions.py
@@ -1,10 +1,23 @@
 import numpy as np
 import glob
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+
+
 
 rundir = '/nobackupp2/gfsnyder/VELA_sunrise/Runs/VELA_v2/VELA28'
 
 
 cam_files = glob.glob(rundir+'/*/input/*cameras')
 
-for i, name in enumerate(cam_files):
+
+fig = figure(figsize = (30,5))
+
+
+for i, name in enumerate(cam_files[0:10]):
 	print name
+	ax = fig.add_subplot(1,10,i+1,, projection='3d')
+
+	data = np.loadtxt(name)
+	for j in arange(len(data[:,0]))
+		axes.plot([0,0,0], [data[j, 0], data[j,1],data[j,2]],'k-')
