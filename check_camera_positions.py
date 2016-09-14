56a8dd805bf97f8cb8847726a739805af4ecd799
RaymondSimons <rsimons@pha.jhu.edu>
2016-09-14 12:13:58 -0400

    testing camera positions from VELA/YT pipeline

--- check_camera_positions.py
+++ check_camera_positions.py
@@ -1,3 +1,10 @@
 import numpy as np
+import glob
+
+rundir = '/nobackupp2/gfsnyder/VELA_sunrise/Runs/VELA_v2/VELA28'
 
 
+cam_files = glob.glob(rundir+'/*/input/*cameras')
+
+for i, name in enumerate(cam_files):
+	print name
