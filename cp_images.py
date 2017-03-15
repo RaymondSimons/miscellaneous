80d34a8ccc8dbacede05e5ee223ac8c4a174c104
RaymondSimons <rsimons@pha.jhu.edu>
2017-03-15 15:25:19 -0400

    for copying images over

--- cp_images.py
+++ cp_images.py
@@ -6,6 +6,8 @@ a = '0.280'
 im_flnames = glob('/nobackupp2/gfsnyder/VELA_sunrise/Runs/VELA_v2/VELA*/*_a%s_sunrise/images/images_*_a%s_sunrise.tar'%(a,a))
 
 for n, fl in enumerate(im_flnames):
+    new_dir = '/nobackupp2/rcsimons/images/'
+    os.system('cp %s %s'(fl, new_dir))
     print n, fl
 
 
