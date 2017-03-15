64b2db4bd204eea4493f64a6d19c964095b3b9ae
RaymondSimons <rsimons@pha.jhu.edu>
2017-03-15 15:20:44 -0400

    for copying images over

---
+++ cp_images.py
@@ -0,0 +1,11 @@
+import glob
+from glob import glob
+import os
+
+
+im_flnames = glob.glob('/nobackupp2/gfsnyder/VELA_sunrise/Runs/VELA_v2/*_a0.350_sunrise/images/images_*_a0.350_sunrise.tar')
+
+for fl in im_flnames:
+    print fl
+
+
