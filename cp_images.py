f803e212a15d2c40af15cb477f771441520e0c2d
RaymondSimons <rsimons@pha.jhu.edu>
2017-03-15 15:23:44 -0400

    for copying images over

--- cp_images.py
+++ cp_images.py
@@ -6,6 +6,6 @@ a = '0.350'
 im_flnames = glob('/nobackupp2/gfsnyder/VELA_sunrise/Runs/VELA_v2/VELA*/*_a%s_sunrise/images/images_*_a%s_sunrise.tar'%(a,a))
 
 for n, fl in enumerate(im_flnames):
-    print fl
+    print n, fl
 
 
