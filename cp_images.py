29a4487062995c275c84dad743411c65534d9dfa
RaymondSimons <rsimons@pha.jhu.edu>
2017-03-15 15:21:16 -0400

    for copying images over

--- cp_images.py
+++ cp_images.py
@@ -3,7 +3,7 @@ from glob import glob
 import os
 
 
-im_flnames = glob.glob('/nobackupp2/gfsnyder/VELA_sunrise/Runs/VELA_v2/*_a0.350_sunrise/images/images_*_a0.350_sunrise.tar')
+im_flnames = glob('/nobackupp2/gfsnyder/VELA_sunrise/Runs/VELA_v2/*_a0.350_sunrise/images/images_*_a0.350_sunrise.tar')
 
 for fl in im_flnames:
     print fl
